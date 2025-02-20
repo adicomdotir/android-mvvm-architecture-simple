63eb0245645a7f71d4ad3455e8324b0156d21f48
adicomdotir <adicom.ir@gmail.com>
2025-02-20 13:03:13 +0330

    refactor: clean home feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "ir.adicom.training"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
